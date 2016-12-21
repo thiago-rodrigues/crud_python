afe340758f2478664324f75c07d5593ed4ecef37
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-12-21 16:37:38 -0200

    adiciona classes no lugar das funções soltas

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,21 +3,31 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
 from django.http import Http404
+from django.views import generic
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'helloworld/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'helloworld/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'helloworld/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'helloworld/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'helloworld/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'helloworld/results.html'
+
+# class SuaView(generic.View):
+    
+#     def get(self, request):
+#         question = Question.objects.get(id=1)
+#         return render(request, 'template.html', locals())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
