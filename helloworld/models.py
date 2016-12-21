9d0ce9f6852e261b2c226fcb248038a271a073b7
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-12-21 16:38:25 -0200

    corrige bug de data de publicação futura

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+import datetime
 from django.db import models
 from django.utils import timezone
 
@@ -8,9 +8,10 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
-
+    
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
