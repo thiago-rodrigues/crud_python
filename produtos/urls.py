2e3391a1ef79d24fcc303eff31bc06b9564f6331
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-12-26 17:23:04 -0200

    adiciona rotas excluir e editar

--- produtos/urls.py
+++ produtos/urls.py
@@ -6,5 +6,9 @@ app_name = 'produtos'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^cadastro/$', views.CadastroView.as_view(), name='cadastro'),
-    url(r'^listar/$', views.ListaView.as_view(), name='listar')
+    url(r'^listar/$', views.ListaView.as_view(), name='listar'),
+    url(r'^excluir/(?P<id_produto>[0-9]+)/$',
+        views.ExcluiProduto.as_view(), name='excluir'),
+    url(r'^editar/(?P<id_produto>[0-9]+)/$',
+        views.EditaProduto.as_view(), name='editar'),
 ]
