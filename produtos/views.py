b771a37ea86e89595a908bf2181edc1f1f3fad7d
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-12-26 10:58:11 -0200

    adiciona função salvar a class cadastro

--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views import View
 from login.views import AuthView
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import Produtos
 
 
 class IndexView(AuthView):
@@ -13,3 +15,13 @@ class CadastroView(AuthView):
 
     def get(self, request):
         return render(request, 'produtos/cadastro.html')
+
+    def post(self, request):
+        novo_produto = Produtos()
+        novo_produto.nome = request.POST['nome']
+        novo_produto.modelo = request.POST['modelo']
+        novo_produto.descricao = request.POST['descricao']
+        novo_produto.save()
+        messages.add_message(request, messages.INFO,
+                             'Produto cadastrado com sucesso!')
+        return HttpResponseRedirect('/produtos/cadastro/')
