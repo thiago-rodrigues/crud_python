d14614eb4edc25d90f84db68c6589ee867785745
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-12-26 17:23:38 -0200

    adiciona view editar e excluir

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from login.views import AuthView
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -33,3 +33,33 @@ class ListaView(AuthView):
         lista_produtos = Produtos.objects.order_by('nome')
         return render(request, 'produtos/listar.html',
                       {'lista_produtos': lista_produtos})
+
+
+class EditaProduto(AuthView):
+
+    def get(self, request, id_produto):
+        produto = get_object_or_404(Produtos, pk=id_produto)
+        return render(request, 'produtos/editar.html', {'produto': produto})
+
+    def post(self, request, id_produto):
+        produto = get_object_or_404(Produtos, pk=id_produto)
+        produto.nome = request.POST['nome']
+        produto.modelo = request.POST['modelo']
+        produto.descricao = request.POST['descricao']
+        produto.save()
+        messages.add_message(request, messages.INFO,
+                             'Produto atualizado com sucesso!')
+        return HttpResponseRedirect('/produtos/cadastro/')
+
+
+class ExcluiProduto(AuthView):
+
+    def get(self, request, id_produto):
+        produto = get_object_or_404(Produtos, pk=id_produto)
+        try:
+            produto.delete()
+        except (KeyError, produto.DoesNotExist):
+            messages.add_message(request, messages.INFO,
+                                 'Erro ao excluir produto.')
+
+        return HttpResponseRedirect('/produtos/listar/')
